95d42506033196e356afee439ad7e90899c450fe
Miroslav Husťák <97535187+MiroslavHustak@users.noreply.github.com>
2024-08-21 13:03:13 +0200

    Add project files.

---
+++ ClientCallTemplates/Program.fs
@@ -0,0 +1,25 @@
+﻿namespace LLM_API
+
+open System
+open FsHttp
+open Newtonsoft.Json
+
+open TestAPI
+
+module API = 
+    
+    //CallRestApiWeather.runRestApiWeather ()
+    
+    CallRestApiWeatherThoth.runRestApiWeatherThoth ()
+
+    Console.ReadKey() |> ignore
+
+    //CallRestApi.runRestApi ()
+
+    //CallRestApiThoth.runRestApiThoth ()
+
+    //Console.ReadKey() |> ignore
+    
+    //CallRpc.runRpc ()
+
+    //Console.ReadKey() |> ignore
